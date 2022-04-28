16f5dcbc573f1b1cb99b9d148884ff4f7663989e
flogan-ultranauts <flogan@ultranauts.co>
2022-04-28 09:28:57 -0400

    shows iteration 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+
 
 function App() {
+  const [password,setPassword]= useState('')
+  const [error,setError]= useState('')
+  const [accept,setAccept]=useState('')
+
+  function handlechange(e) {
+    setPassword(e.target.value);
+
+    const letterCount = (password.match(/[A-Za-z]+/g) ?? []).length
+    const numberCount =(password.match(/[0-9]+/g) ?? []).length
+
+    if(password.length < 8 || letterCount <1 || numberCount <1){
+      setError('password is not accepted')
+      setAccept(null);
+    }else if(password.length >= 8 && letterCount > 0 && numberCount > 0){
+      setError(null);
+      setAccept("password is accepted")
+    }
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex item-center flex-col justify-between py-80">
+      <div>
+
+     <input type="password" value={password} onChange={handlechange} placeholder='enter your password' />
+      </div>
+      <p className='text-red-500 text-sm font-normal'>
+
+      {error}
+      </p>
+
+      <p className='text-green-500 text-lg font-semibold '>
+
+      {accept}
+      </p>
     </div>
   );
 }
